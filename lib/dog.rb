ab53e2a48c07b1ede32c55a3e78bbd09c8a96640
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-05 05:02:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,15 @@
 class Dog 
-
-  def name=(dog_name) 
-    @this_dogs_name = dog_name 
+ 
+  def initialize(name)
+    @name = name
   end
-  
+ 
   def name
-    @this_dogs_name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
   end
 
-  def breed=(name)
-    @breed = name
-  end
-  
-  def breed 
-    @breed
-  end
-    
 end
-
- 
- 
-
-  
